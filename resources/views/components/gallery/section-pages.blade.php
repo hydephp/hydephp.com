0ac0791809d8294b6f3c6c21ae1f40e1940589e8
Caen De Silva <caen@desilva.se>
2022-06-22 20:21:02 +0200

    Tweak spacing

--- resources/views/components/gallery/section-pages.blade.php
+++ resources/views/components/gallery/section-pages.blade.php
@@ -1,7 +1,7 @@
-<section id="pages-section-wrapper" class="mx-auto items-center text-center py-16 px-4 lg:min-h-screen bg-slate-100 dark:bg-slate-800">
+<section id="pages-section-wrapper" class="mx-auto items-center text-center py-16 px-4 bg-slate-100 dark:bg-slate-800">
 	<div  id="pages-section">
 		
-	<div id="slide1" class="slide mx-auto items-center lg:min-h-screen">
+	<div id="slide1" class="slide mx-auto items-center">
 		<h1 class="text-2xl md:3-xl lg:text-5xl font-black text-slate-700 dark:text-white px-3 my-3">
 			<div id="pagesSectionHeader">
 				Create Markdown Driven Pages
@@ -22,7 +22,7 @@
 			<a href="https://github.com/hydephp/examples/blob/24218d98cf86aea217729337ad80801d6930f5a0/examples/markdown-pages/hyde-demo-markdown-page.md">View source on GitHub</a>
 		</p>
 	</div>
-	<div id="slide2" class="slide mx-auto items-center">
+	<div id="slide2" class="slide mx-auto items-center mt-32">
 		<h1 class="text-2xl md:3-xl lg:text-5xl font-black text-slate-700 dark:text-white px-3 my-3 mt-20">
 			<div id="pagesSectionHeader">
 				Or use Laravel's Blade
