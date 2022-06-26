a7548c2ce192c293c223c9f0c13b5d0a52e070bc
Caen De Silva <caen@desilva.se>
2022-06-26 22:54:03 +0200

    Add ARIA roles to list as Tailwind removes them

--- resources/views/components/footer-links.blade.php
+++ resources/views/components/footer-links.blade.php
@@ -1,15 +1,15 @@
-<ul class="not-prose whitespace-nowrap sm:justify-center flex-wrap flex-row flex sm:items-center  mt-2 mx-1 text-sm text-gray-500 dark:text-gray-300  sm:mr-4 sm:pr-4 list-none pl-0 ml-0 sm:pl-4 sm:ml-4 border-t border-gray-300 dark:border-gray-700">
-	<li class="my-2 mx-1">
+<ul class="not-prose whitespace-nowrap sm:justify-center flex-wrap flex-row flex sm:items-center  mt-2 mx-1 text-sm text-gray-500 dark:text-gray-300  sm:mr-4 sm:pr-4 list-none pl-0 ml-0 sm:pl-4 sm:ml-4 border-t border-gray-300 dark:border-gray-700" role="list">
+	<li class="my-2 mx-1" role="listitem">
 		<a href="{{ Hyde::relativeLink('community.html', $currentPage) }}" class="text-gray-500 dark:text-gray-300   dark:hover:text-gray-100   hover:text-gray-900 flex items-center sm:mx-2">
 			Resources
 		</a>
 	</li>
-	<li class="my-2 mx-1">
+	<li class="my-2 mx-1" role="listitem">
 		<a href="{{ Hyde::relativeLink(Hyde::docsIndexPath(), $currentPage) }}" class="text-gray-500 dark:text-gray-300   dark:hover:text-gray-100   hover:text-gray-900 flex items-center sm:mx-2">
 			Documentation
 		</a>
 	</li>
-	<li class="my-2 mx-1">
+	<li class="my-2 mx-1" role="listitem">
 		<a href="{{ Hyde::relativeLink('sitemap.html', $currentPage) }}" class="text-gray-500 dark:text-gray-300   dark:hover:text-gray-100   hover:text-gray-900 flex items-center sm:mx-2">
 			Sitemap
 		</a>
@@ -17,12 +17,12 @@
 	<li class="my-2 mx-1" role="presentation">
 		<span class="sm:mr-2 opacity-50 select-none">|</span>
 	</li>
-	<li class="my-2 mx-1">
+	<li class="my-2 mx-1" role="listitem">
 		<a href="https://github.com/hydephp/" class="text-gray-500 dark:text-gray-300   dark:hover:text-gray-100   hover:text-gray-900 flex items-center">
 			GitHub
 		</a>
 	</li>
-	<li class="my-2 mx-1">
+	<li class="my-2 mx-1" role="listitem">
 		<a href="https://www.buymeacoffee.com/caen" class="text-gray-500 dark:text-gray-300   dark:hover:text-gray-100   hover:text-gray-900 flex items-center sm:mx-2">
 			Sponsor
 		</a>
