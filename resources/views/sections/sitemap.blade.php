b4396c7152137e45b6ec5e1ecbb8432facd35bad
Caen De Silva <caen@desilva.se>
2022-06-26 22:49:40 +0200

    Display lists side by side

--- resources/views/sections/sitemap.blade.php
+++ resources/views/sections/sitemap.blade.php
@@ -15,19 +15,25 @@ unset ($pages[array_search('404', $pages)]);
 
 @endphp
 
-<h2>Main Pages</h2>
-
-<ul>
-	@foreach($pages as $page)
-	<li><a href="{{ $page }}">{{ Hyde::titleFromSlug($page) }}</a></li>
-	@endforeach
-</ul>
-
-
-<h2>Documentation Pages</h2>
-
-<ul>
-	@foreach(CollectionService::getDocumentationPageList() as $page)
-	<li><a href="{{ $page }}">{{ Hyde::titleFromSlug($page) }}</a></li>
-	@endforeach
-</ul>
+<div class="flex flex-wrap">
+	<section class="pr-20 lg:pr-32">
+		<h2>Main Pages</h2>
+	
+		<ul>
+			@foreach($pages as $page)
+			<li><a href="{{ $page }}">{{ Hyde::titleFromSlug($page) }}</a></li>
+			@endforeach
+		</ul>
+	</section>
+	
+	
+	<section class="pr-20 lg:pr-32">
+		<h2>Documentation Pages</h2>
+	
+		<ul>
+			@foreach(CollectionService::getDocumentationPageList() as $page)
+			<li><a href="{{ $page }}">{{ Hyde::titleFromSlug($page) }}</a></li>
+			@endforeach
+		</ul>
+	</section>
+</div>
