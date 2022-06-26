7277f380c9d0e677cc335a19f469f5a06ac2cc7f
Caen De Silva <caen@desilva.se>
2022-06-26 23:03:39 +0200

    Add the HydePHP logo to the navigation menu

---
+++ resources/views/vendor/hyde/components/navigation/navigation-brand.blade.php
@@ -0,0 +1,4 @@
+<a aria-label="Home page link" href="{{ $homeRoute }}" class="font-bold px-4">
+	<img src="/logo-masked32-min.png" alt="HydePHP Logo" class="inline">
+	{{ config('hyde.name', 'HydePHP') }}
+</a>
