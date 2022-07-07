56fef7641b3d44a52b509acd4ed94a0c4a27131e
Caen De Silva <caen@desilva.se>
2022-07-07 10:30:52 +0200

    Limit to 3 posts

--- resources/views/sections/posts.blade.php
+++ resources/views/sections/posts.blade.php
@@ -1,5 +1,5 @@
-<section id="posts" class="mx-auto max-w-7xl py-16 px-8 lg:mt-8">
-	<header class="lg:mb-12 xl:mb-16">
+<section id="posts" class="w-screen py-16  lg:mt-8 relative">
+	<header class="lg:mb-12 xl:mb-16 max-w-7xl mx-auto">
 		<h2 class="text-3xl text-left opacity-75 dark:opacity-90 leading-10 tracking-tight font-extrabold sm:leading-none sm:mb-8 md:text-4xl md:text-center lg:text-5xl">
 			Latest Posts
 		</h2>
@@ -8,8 +8,16 @@
 		</p>
 	</header>
 	<div class="max-w-xl mx-auto">
-		@foreach(MarkdownPost::getLatestPosts() as $post)
+		@foreach(MarkdownPost::getLatestPosts()->take(3) as $post)
 		@include('hyde::components.article-excerpt')
 		@endforeach
 	</div>
+	
+	<footer class="lg:mb-12 xl:mb-16 max-w-7xl mx-auto">
+		<p class="sm:text-center text-lg mx-auto mt-4 mb-8 prose">
+			See more on the <a href="posts">posts page</a>!
+		</p>
+	</footer>
+	<svg class="absolute bottom-0 w-full text-black fill-current" viewBox="0 0 1400 74" xmlns="http://www.w3.org/2000/svg"><path d="M0 24C87.243 11.422 173.12 5.133 257.633 5.133 468.305 5.133 578.027 74 700 74c136.015 0 290.882-96.208 481.234-68.867C1268.807 17.71 1341.73 24 1400 24v50H0V24z" /></svg>
+
 </section>
