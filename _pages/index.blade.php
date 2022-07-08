0d74b2c3d088deb0cad0595478d7e5ed6a205f34
Caen De Silva <caen@desilva.se>
2022-07-08 10:39:33 +0200

    Make testimonials more responsive

--- _pages/index.blade.php
+++ _pages/index.blade.php
@@ -265,11 +265,10 @@
             </div>
             <div class="flex flex-col items-center justify-center max-w-2xl py-8 mx-auto xl:flex-row xl:max-w-full">
                 <div class="w-full xl:w-1/2 xl:pr-8">
-                    <blockquote
-                    class="flex flex-col-reverse items-center justify-between w-full col-span-1 p-6 text-center transition-all duration-200 bg-gray-100 rounded-lg md:flex-row md:text-left hover:bg-gray-50 hover:shadow ease">
-                    <div class="flex flex-col pr-8">
-                        <div class="relative pl-12">
-                            <svg class="absolute left-0 w-10 h-10 text-indigo-500 fill-current"
+                    <blockquote class="my-4 flex flex-col-reverse items-center justify-between w-full col-span-1 p-6 text-center transition-all duration-200 bg-gray-100 rounded-lg md:flex-row md:text-left hover:bg-gray-50 hover:shadow ease">
+                    <div class="flex flex-col sm:pr-8">
+                        <div class="relative sm:pl-12">
+                            <svg class="hidden sm:block absolute left-0 w-10 h-10 text-indigo-500 fill-current"
                             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 125">
                             <path
                             d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z" />
@@ -280,21 +279,20 @@
                             ⭐️⭐️⭐️⭐️
                         </p>
                     </div>
-                    <h3 class="pl-12 mt-3 text-base font-medium leading-5 text-gray-800 truncate">
+                    <h3 class="pl-12 mt-3 text-base font-medium leading-5 text-gray-800 ">
                         <a href="https://twitter.com/SavvasStephnds" rel="author nofollow">@SavvasStephnds</a>
-                        <span class="mt-1 text-sm leading-5 text-gray-500 truncate">- <a href="https://twitter.com/SavvasStephnds/status/1534928318503391233?ref_src=twsrc%5Etfw">Via Twitter</a></span>
+                        <span class="mt-1 text-sm leading-5 text-gray-500 ">- <a href="https://twitter.com/SavvasStephnds/status/1534928318503391233?ref_src=twsrc%5Etfw">Via Twitter</a></span>
                     </h3>
-                    <p class="mt-1 text-sm leading-5 text-gray-500 truncate"></p>
+                    <p class="mt-1 text-sm leading-5 text-gray-500 "></p>
                 </div>
                 <img class="flex-shrink-0 object-cover w-24 h-24 mb-5 bg-gray-300 rounded-full md:mb-0"
                 src="https://pbs.twimg.com/profile_images/1479839481821904899/h2z7ovvl_200x200.jpg"
                 alt="Profile image">
             </blockquote>
-            <blockquote
-            class="flex flex-col-reverse items-center justify-between w-full col-span-1 p-6 mt-16 text-center transition-all duration-200 bg-gray-100 rounded-lg md:flex-row md:text-left hover:bg-gray-50 hover:shadow ease">
-            <div class="flex flex-col pr-10">
-                <div class="relative pl-12">
-                    <svg class="absolute left-0 w-10 h-10 text-indigo-500 fill-current"
+            <blockquote class="my-4 hidden lg:flex flex-col-reverse items-center justify-between w-full col-span-1 p-6 mt-12 text-center transition-all duration-200 bg-gray-100 rounded-lg md:flex-row md:text-left hover:bg-gray-50 hover:shadow ease">
+            <div class="flex flex-col sm:pr-10">
+                <div class="relative sm:pl-12">
+                    <svg class="hidden sm:block absolute left-0 w-10 h-10 text-indigo-500 fill-current"
                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 125">
                     <path
                     d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z" />
@@ -304,11 +302,11 @@
                     <a href="https://twitter.com/hashtag/HydePHP">#HydePHP</a>!
                 </p>
             </div>
-            <h3 class="pl-12 mt-3 text-base font-medium leading-5 text-gray-800 truncate">
-                This could be you! <span class="mt-1 text-sm leading-5 text-gray-500 truncate">- CEO
+            <h3 class="pl-12 mt-3 text-base font-medium leading-5 text-gray-800 ">
+                This could be you! <span class="mt-1 text-sm leading-5 text-gray-500 ">- CEO
                     SomeCompany</span>
                 </h3>
-                <p class="mt-1 text-sm leading-5 text-gray-500 truncate"></p>
+                <p class="mt-1 text-sm leading-5 text-gray-500 "></p>
             </div>
             <img class="flex-shrink-0 object-cover w-24 h-24 mb-5 bg-gray-300 rounded-full md:mb-0"
             src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
@@ -316,11 +314,10 @@
         </blockquote>
     </div>
     <div class="w-full xl:w-1/2 xl:pl-8">
-        <blockquote
-        class="flex flex-col-reverse items-center justify-between w-full col-span-1 p-6 text-center transition-all duration-200 bg-gray-100 rounded-lg md:flex-row md:text-left hover:bg-gray-50 hover:shadow ease">
-        <div class="flex flex-col pr-10">
-            <div class="relative pl-12">
-                <svg class="absolute left-0 w-10 h-10 text-indigo-500 fill-current"
+        <blockquote class="my-4 flex flex-col-reverse items-center justify-between w-full col-span-1 p-6 text-center transition-all duration-200 bg-gray-100 rounded-lg md:flex-row md:text-left hover:bg-gray-50 hover:shadow ease">
+        <div class="flex flex-col sm:pr-10">
+            <div class="relative sm:pl-12">
+                <svg class="hidden sm:block absolute left-0 w-10 h-10 text-indigo-500 fill-current"
                 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 125">
                 <path
                 d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z" />
@@ -332,21 +329,20 @@
                 Project: 10/10
             </p>
         </div>
-        <h3 class="pl-12 mt-3 text-base font-medium leading-5 text-gray-800 truncate">
+        <h3 class="pl-12 mt-3 text-base font-medium leading-5 text-gray-800 ">
             <a href="https://twitter.com/peteralexbizjak" rel="author nofollow">@peteralexbizjak</a>
-            <span class="mt-1 text-sm leading-5 text-gray-500 truncate">- <a href="https://twitter.com/peteralexbizjak/status/1535177275649536001">Via Twitter</a></span>
+            <span class="mt-1 text-sm leading-5 text-gray-500 ">- <a href="https://twitter.com/peteralexbizjak/status/1535177275649536001">Via Twitter</a></span>
         </h3>
-        <p class="mt-1 text-sm leading-5 text-gray-500 truncate"></p>
+        <p class="mt-1 text-sm leading-5 text-gray-500 "></p>
     </div>
     <img class="flex-shrink-0 object-cover w-24 h-24 mb-5 bg-gray-300 rounded-full md:mb-0"
     src="https://pbs.twimg.com/profile_images/1543562487320268805/iCrWcyG-_200x200.jpg"
     alt="Profile image">
 </blockquote>
-<blockquote
-class="flex flex-col-reverse items-center justify-between w-full col-span-1 p-6 mt-16 text-center transition-all duration-200 bg-gray-100 rounded-lg md:flex-row md:text-left hover:bg-gray-50 hover:shadow ease">
-<div class="flex flex-col pr-10">
-    <div class="relative pl-12">
-        <svg class="absolute left-0 w-10 h-10 text-indigo-500 fill-current"
+<blockquote class="my-4 flex flex-col-reverse items-center justify-between w-full col-span-1 p-6 mt-12 text-center transition-all duration-200 bg-gray-100 rounded-lg md:flex-row md:text-left hover:bg-gray-50 hover:shadow ease">
+<div class="flex flex-col sm:pr-10">
+    <div class="relative sm:pl-12">
+        <svg class="hidden sm:block absolute left-0 w-10 h-10 text-indigo-500 fill-current"
         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 125">
         <path
         d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z" />
@@ -356,11 +352,11 @@ class="flex flex-col-reverse items-center justify-between w-full col-span-1 p-6 
         <a href="https://twitter.com/hashtag/HydePHP">#HydePHP</a>!
     </p>
 </div>
-<h3 class="pl-12 mt-3 text-base font-medium leading-5 text-gray-800 truncate">
-    This could be you! <span class="mt-1 text-sm leading-5 text-gray-500 truncate">- CEO
+<h3 class="pl-12 mt-3 text-base font-medium leading-5 text-gray-800 ">
+    This could be you! <span class="mt-1 text-sm leading-5 text-gray-500 ">- CEO
         SomeCompany</span>
     </h3>
-    <p class="mt-1 text-sm leading-5 text-gray-500 truncate"></p>
+    <p class="mt-1 text-sm leading-5 text-gray-500 "></p>
 </div>
 <img class="flex-shrink-0 object-cover w-24 h-24 mb-5 bg-gray-300 rounded-full md:mb-0"
 src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
