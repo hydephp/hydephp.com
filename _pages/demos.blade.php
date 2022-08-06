42827041d894d68ada59202e9b0ce8649f740641
Caen De Silva <caen@desilva.se>
2022-08-06 17:44:25 +0200

    Update demos.blade.php

--- _pages/demos.blade.php
+++ _pages/demos.blade.php
@@ -6,15 +6,18 @@
 	<header>
 		<h1 class="text-center mb-5 text-2xl font-black leading-tight xl:text-4xl">Live Demos</h1>
 		<div class="prose dark:prose-invert mx-auto text-center">
-			<p>Have an idea for a demo? Want to submit yours? <a href="https://github.com/hydephp/hydephp.com">Make a pull request</a>!</p>
+			<p class="lead mb-3">Wondering what a HydePHP site looks like?</p>
+			<p class="mt-3">
+				Here are some live demo websites built with Hyde that you can try out!
+			</p>
 		</div>
 	</header>
 
 
 	<div class="flex flex-col items-center justify-center py-10 sm:flex-row">
-		<div class="p-4 px-6 sm:w-1/2 lg:w-1/3">
+		<div class="py-4 sm:px-6 w-96 max-w-full">
 			<div class="flex flex-col overflow-hidden bg-slate-100 dark:bg-slate-900 rounded-lg shadow-lg hover:shadow-xl transition-shadow">
-				<a href="https://hydephp.github.io/portfolio-demo"><img class="h-56 rounded-t-lg" alt="article image"
+				<a href="https://hydephp.github.io/portfolio-demo"><img class="h-56 rounded-t-lg m-0 w-full" alt="article image"
 					src="/media/portfolio-composite-min.png"></a>
 				<div class="px-6 pt-4 mb-2 text-xl font-bold">
 					<span>Markdown Portfolio Site</span>
@@ -29,9 +32,26 @@
 				</div>
 			</div>
 		</div>
-		{{-- <div class="p-4 px-6 sm:w-1/2 lg:w-1/3">
+		<div class="py-4 sm:px-6 w-96 max-w-full">
 			<div class="flex flex-col overflow-hidden bg-slate-100 dark:bg-slate-900 rounded-lg shadow-lg hover:shadow-xl transition-shadow">
-				<img class="h-56 rounded-t-lg" alt="article image"
+				<a href="https://hydephp.com/"><img class="h-56 rounded-t-lg m-0 w-full" alt="article image"
+					src="/media/hydephp-com-composite-min.png"></a>
+				<div class="px-6 pt-4 mb-2 text-xl font-bold">
+					<span>HydePHP.com</span>
+				</div>
+				<div class="px-6 pt-2">
+					<small>By HydePHP.com | 2022-06-22</small>
+					<div class="overflow-hidden h-16 mt-2">The very same site you are currently browsing!</div>
+				</div>
+				<div class="px-6 pb-4 text-center mt-3">
+					<a href="https://github.com/hydephp/hydephp.com" class="inline-block px-3 py-1 my-1 mr-2 text-sm font-semibold text-white bg-gray-400 dark:bg-gray-500 rounded-full">GitHub Source Code</a>
+					<a href="https://hydephp.com/" class="inline-block px-3 py-1 my-1 mr-2 text-sm font-semibold text-white bg-indigo-500 dark:bg-indigo-400 rounded-full">Live Demo</a>
+				</div>
+			</div>
+		</div>
+		{{-- <div class="py-4 sm:px-6 w-96 max-w-full">
+			<div class="flex flex-col overflow-hidden bg-slate-100 dark:bg-slate-900 rounded-lg shadow-lg hover:shadow-xl transition-shadow">
+				<img class="h-56 rounded-t-lg m-0 w-full" alt="article image"
 					src="https://images.unsplash.com/photo-1593720219276-0b1eacd0aef4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1343&q=80">
 				<div class="px-6 pt-4 mb-2 text-xl font-bold">
 					<span>More coming soon!</span>
@@ -47,6 +67,11 @@
 			</div>
 		</div> --}}
 	</div>
+	<footer>
+		<div class="prose dark:prose-invert mx-auto text-center">
+			<p>Have an idea for a demo? Want to submit yours? <a href="https://github.com/hydephp/hydephp.com">Make a pull request</a>!</p>
+		</div>
+	</footer>
 </main>
 
 @endsection
