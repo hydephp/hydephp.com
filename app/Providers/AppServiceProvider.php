33621a081e1e70e7586c822d55a94ea8be1acf4a
Caen De Silva <caen@desilva.se>
2023-10-31 22:03:34 +0100

    Add redirect from blog to posts
    In case someone visits /blog, this no longer gives a 404

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Hyde;
+use Hyde\Support\Models\Redirect;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->addRedirect(new Redirect('blog', 'posts'));
+    }
+
+    protected function addRedirect(Redirect $redirect): void
+    {
+        Hyde::pages()->addPage($redirect);
+        Hyde::routes()->addRoute($redirect->getRoute());
     }
 }
