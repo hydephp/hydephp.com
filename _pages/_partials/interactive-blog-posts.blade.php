06979cfb7d2be5745472aa64f3d6ef5a691ad305
Caen De Silva <caen@desilva.se>
2023-11-04 10:32:26 +0100

    Create interactive-blog-posts.blade.php

---
+++ _pages/_partials/interactive-blog-posts.blade.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        #argon-21119f2 pre{border-radius:.25rem;margin-top:1rem;margin-bottom:1rem;overflow-x:auto}#argon-21119f2 pre code.torchlight{display:block;min-width:-webkit-max-content;min-width:-moz-max-content;min-width:max-content;padding-top:1rem;padding-bottom:1rem}#argon-21119f2 pre code.torchlight .line{padding-left:1rem;padding-right:1rem}#argon-21119f2 pre code.torchlight .line-number{margin-right:1rem}.argon-card{min-width:320px;max-width:800px;border-radius:16px;overflow:hidden;background:#292d3e;width:fit-content}.argon-card article,code,menu,pre{margin:0;padding:0}.argon-card pre>code{font-family:'Fira Code',monospace}.argon-card-header{padding:8px 4px;background:#212529;color:#fff;display:flex;align-items:center}.argon-card-header h1{font-size:14px;font-weight:400;margin:0 auto;color:rgba(255,255,255,.75);font-family:sans-serif}.argon-card-header menu{user-select:none;margin-left:8px;float:left;position:absolute}.argon-card-header menu button{all:unset;display:inline-block;width:12px;height:12px;border-radius:50%;cursor:pointer;opacity:.75}.argon-card-header menu button:hover{opacity:1}.argon-card-header menu button.red{background-color:#f3615a}.argon-card-header menu button.yellow{background-color:#f4c036}.argon-card-header menu button.green{background-color:#3ccb3e}
+        #argon-df2fd49 {
+            height: fit-content;
+            background: white;
+        }
+        #argon-df2fd49 .argon-header {
+            padding: 6px 4px;
+            font-size: 10px;
+            background: #343434;
+        }
+        #argon-df2fd49 .argon-header menu button {
+            width: 8px;
+            height: 8px;
+        }
+        #argon-21119f2 {
+            z-index: 1;
+            font-size: 50%;
+        }
+        #argon-21119f2 header {
+            padding: 4px 4px;
+            font-size: 10px;
+        }
+        #argon-21119f2 header menu button {
+            width: 8px;
+            height: 8px;
+        }
+
+        @media (min-width: 1000px) {
+            #argon-21119f2 {
+                font-size: 62.5%;
+            }
+        }
+        @media (min-width: 1400px) {
+            #argon-21119f2 {
+                font-size: 75%;
+                margin-top: 40px;
+            }
+        }
+        .torchlight {
+            padding-top: 0!important;
+            padding-bottom: 0!important;
+        }
+        .shadow-2xl {
+            box-shadow: var(--tw-ring-offset-shadow,0 0 #0000),0 0 #0000 ,var(--tw-shadow);
+            --tw-shadow: 0 25px 50px -12px rgba(0,0,0,.25);
+            --tw-shadow-colored: 0 25px 50px -12px 0 0 #0000;
+        }
+        main {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: center;
+            min-height: 100vh;
+            background: #f4f4f4;
+        }
+        #result {
+            height: 500px;
+            width: 700px;
+            overflow: hidden;
+            margin-bottom: 1rem!important;
+        }
+        #result > div {
+            /* make all text smaller */
+            transform: scale(0.8);
+            transform-origin: top left;
+            width: 125%;
+        }
+        #result #content {
+            padding: 1rem 5rem;
+        }
+    </style>
+</head>
+<body>
+<main>
+    <section class="argon-card text-left shadow-2xl" id="argon-21119f2">
+        <header class="argon-card-header">
+            <menu> <button class="red"></button> <button class="yellow"></button> <button class="green"></button> </menu>
+            <h1 style="font-size: 10px;">_posts/hello-world.md</h1>
+        </header>
+        <article>
+            <pre><code data-theme="material-theme-palenight" data-lang="markdown" class="torchlight" style="background-color: #292D3E; --theme-selection-background: #00000080;"><!-- Syntax highlighted by torchlight.dev --><div class='line'><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number"> 1</span><span style="color: #A6ACCD;">---</span></div><div class='line'><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number"> 2</span><span style="color: #F07178;">title</span><span style="color: #89DDFF;">:</span><span style="color: #A6ACCD;"> </span><span style="color: #C3E88D;">Hello World!</span></div><div class='line'><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number"> 3</span><span style="color: #F07178;">description</span><span style="color: #89DDFF;">:</span><span style="color: #A6ACCD;"> </span><span style="color: #C3E88D;">Short post excerpt for previews and meta tags</span></div><div class='line'><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number"> 4</span><span style="color: #F07178;">category</span><span style="color: #89DDFF;">:</span><span style="color: #A6ACCD;"> </span><span style="color: #C3E88D;">demo</span></div><div class='line'><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number"> 5</span><span style="color: #F07178;">author</span><span style="color: #89DDFF;">:</span><span style="color: #A6ACCD;"> </span><span style="color: #C3E88D;">mr_hyde</span></div><div class='line'><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number"> 6</span><span style="color: #F07178;">date</span><span style="color: #89DDFF;">:</span><span style="color: #A6ACCD;"> </span><span style="color: #C3E88D;">2022-03-29 09:16</span></div><div class='line'><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number"> 7</span><span style="color: #A6ACCD;">---</span></div><div class='line'><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number"> 8</span>&nbsp;</div><div class='line'><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number"> 9</span><span style="color: #89DDFF;">## </span><span style="color: #FFCB6B;">Write something awesome.</span></div><div class='line'><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">10</span>&nbsp;</div><div class='line'><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">11</span><span style="color: #A6ACCD;">Lorem markdownum Austri occupat redire sum sponte arcus,</span></div><div class='line'><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">12</span><span style="color: #89DDFF;">[</span><span style="color: #C3E88D;">ferae</span><span style="color: #89DDFF;">]</span><span style="color: #89DDFF;">(</span><span style="color: #F07178;">https://www.example.com/hello-world</span><span style="color: #89DDFF;">)</span><span style="color: #A6ACCD;"> longo,</span></div><div class='line'><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">13</span><span style="color: #A6ACCD;">timuit magnanimus aera, violentam. Tractu ter.</span></div><div class='line'><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">14</span>&nbsp;</div><div class='line'><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">15</span><span style="color: #89DDFF;">1.</span><span style="color: #A6ACCD;"> Pelopeia et terras iussa cavernas</span></div><div class='line'><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">16</span><span style="color: #89DDFF;">2.</span><span style="color: #A6ACCD;"> Petit ignoscite ac nuda miserum Tereus</span></div><div class='line'><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">17</span><span style="color: #89DDFF;">3.</span><span style="color: #A6ACCD;"> Tuli facinus Panaque virgo sentire copia</span></div></code></pre>
+        </article>
+    </section>
+
+    <section class="argon-card shadow-2xl" id="argon-df2fd49" style="margin-left: -2rem">
+        <header class="argon-card-header">
+            <menu> <button class="red"></button> <button class="yellow"></button> <button class="green"></button> </menu>
+            <h1 style="font-size: 10px;">_site/posts/hello-world.html</h1>
+        </header>
+        <figure id="result" style="margin: 0; background: white;">
+            {!! file_get_contents(Hyde::path('_media/hello-world.html')) !!}
+        </figure>
+    </section>
+</main>
+</body>
+</html>
