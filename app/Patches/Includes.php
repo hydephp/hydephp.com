4172857e71fbd351ac610f93deea88a6ea689367
Caen De Silva <caen@desilva.se>
2024-06-05 16:51:15 +0200

    Revert "Add Yaml includes helper"
    This reverts commit 3101d3dabae242f28e18608c9e65983a09b554dc.

--- app/Patches/Includes.php
+++ app/Patches/Includes.php
@@ -4,21 +4,10 @@ declare(strict_types=1);
 
 namespace App\Patches;
 
-use Symfony\Component\Yaml\Yaml;
-
 /**
  * @internal Experimental class for added include types not yet added to the Hyde core.
  */
 class Includes extends \Hyde\Support\Includes
 {
-    public static function yaml(string $filename, ?array $default = null): ?array
-    {
-        $path = static::path(basename($filename, '.yml').'.yml');
-
-        if (! file_exists($path)) {
-            return $default === null ? null : $default;
-        }
-
-        return Yaml::parseFile($path);
-    }
+    //
 }
