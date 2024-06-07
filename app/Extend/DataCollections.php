8f52671535d48fad1ca508bf512c31dc4454a3b3
Caen De Silva <caen@desilva.se>
2024-06-07 17:45:15 +0200

    Draft logic redirect

--- app/Extend/DataCollections.php
+++ app/Extend/DataCollections.php
@@ -11,6 +11,10 @@ class DataCollections extends \Hyde\Support\DataCollections
 {
     public static function yaml(string $name): static
     {
+        if (static::hasType($name)) {
+            return static::getTypedYaml($name);
+        }
+
         return parent::yaml($name);
     }
 }
