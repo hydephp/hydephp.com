e7d2410a3636ea304a3cd98b94ad2a03111ca33b
Caen De Silva <caen@desilva.se>
2024-06-07 17:40:27 +0200

    Document interface and feature

--- app/Extend/Concerns/DataCollectionType.php
+++ app/Extend/Concerns/DataCollectionType.php
@@ -2,6 +2,13 @@
 
 namespace App\Extend\Concerns;
 
+/**
+ * Contract for a custom data collection types.
+ *
+ * Types are added by adding a `type.php` file in the collection directory.
+ * The type should return a new anonymous class. While they are currently not checked against this type,
+ * it's recommended to implement this interface to ensure compatible constructors.
+ */
 interface DataCollectionType
 {
     public function __construct(array $data);
