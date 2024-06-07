501c955618f307713c5e5c96f9a038bccaf4e2f7
Caen De Silva <caen@desilva.se>
2024-06-07 17:33:29 +0200

    Create DataCollections.php

---
+++ app/Extend/DataCollections.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Extend;
+
+/**
+ * Experimental typed data collections class extension.
+ */
+class DataCollections extends \Hyde\Support\DataCollections
+{
+    //
+}
