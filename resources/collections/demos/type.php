2a193e8ba51b1583b16b9bd770dfbe76ef8dcc6c
Caen De Silva <caen@desilva.se>
2024-06-07 17:43:50 +0200

    Implement base class structure

--- resources/collections/demos/type.php
+++ resources/collections/demos/type.php
@@ -2,5 +2,22 @@
 
 return new class implements \App\Extend\Concerns\DataCollectionType
 {
-    //
+    private string $title;
+    private string $description;
+    private string $author;
+    private string $date;
+    private string $image;
+    private string $url;
+    private ?string $source;
+
+    public function __construct(array $data)
+    {
+        $this->title = $data['title'];
+        $this->description = $data['description'];
+        $this->author = $data['author'];
+        $this->date = $data['date'];
+        $this->image = $data['image'];
+        $this->url = $data['url'];
+        $this->source = isset($data['source']) ? $data['source'] : null;
+    }
 };
