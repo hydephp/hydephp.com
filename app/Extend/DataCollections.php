5153d2711be53d725e936842fe0d2cb17b111f2d
Caen De Silva <caen@desilva.se>
2024-06-08 12:19:20 +0200

    Revert "Refactor testimonials section to use data collections"

--- app/Extend/DataCollections.php
+++ app/Extend/DataCollections.php
@@ -5,10 +5,8 @@ declare(strict_types=1);
 namespace App\Extend;
 
 use Hyde\Facades\Filesystem;
-use Illuminate\Support\Collection;
 use Hyde\Markdown\Models\FrontMatter;
 use Illuminate\Support\Str;
-use Hyde\Markdown\Models\MarkdownDocument;
 
 /**
  * @experimental Typed data collections class extension.
@@ -24,51 +22,15 @@ class DataCollections extends \Hyde\Support\DataCollections
         return parent::yaml($name);
     }
 
-    public static function markdown(string $name): static
-    {
-        if (static::hasType($name)) {
-            return static::getTypedMarkdown($name);
-        }
-
-        return parent::markdown($name);
-    }
-
     protected static function hasType(string $name): bool
     {
         return Filesystem::exists(self::getTypePath($name)) || class_exists(self::getTypeClassname($name));
     }
 
-    /** @return static<\App\Extend\Concerns\DataCollectionType> */
+    /**
+     * @return static<\App\Extend\Concerns\DataCollectionType>
+     */
     protected static function getTypedYaml(string $name): static
-    {
-        $className = self::getCallableTypeClassName($name);
-
-        return parent::yaml($name)->map(fn (FrontMatter $data) => new $className($data->toArray()));
-    }
-
-    /** @return static<\App\Extend\Concerns\DataCollectionType> */
-    protected static function getTypedMarkdown(string $name): static
-    {
-        $className = self::getCallableTypeClassName($name);
-        return parent::markdown($name)->reject(fn (MarkdownDocument $document, string $key) => basename($key) === 'README.md') // Patch until https://github.com/hydephp/develop/issues/1716
-            ->map(fn (MarkdownDocument $document) => new $className(array_merge(
-                $document->matter()->toArray(),
-                ['markdown' => $document->markdown()->body()]
-            )));
-    }
-
-    protected static function getTypePath(string $name): string
-    {
-        return sprintf('%s/%s/type.php', static::$sourceDirectory, $name);
-    }
-
-    protected static function getTypeClassname(string $name): string
-    {
-        return 'App\\DataCollections\\Types\\'.Str::studly($name);
-    }
-
-    /** @return class-string<\App\Extend\Concerns\DataCollectionType> */
-    protected static function getCallableTypeClassName(string $name): string
     {
         // Load the anonymous class and turn it into a new runtime class
         $className = self::getTypeClassname($name);
@@ -80,13 +42,17 @@ class DataCollections extends \Hyde\Support\DataCollections
             class_alias($newClassName, $className);
         }
 
-        return $className;
+        /** @var \App\Extend\Concerns\DataCollectionType $className */
+        return parent::yaml($name)->map(fn (FrontMatter $data) => new $className($data->toArray()));
     }
 
-    protected static function findFiles(string $name, array|string $extensions): Collection
+    protected static function getTypePath(string $name): string
     {
-        // Depends on https://github.com/hydephp/develop/issues/1716
+        return sprintf('%s/%s/type.php', static::$sourceDirectory, $name);
+    }
 
-        return parent::findFiles($name, $extensions)->reject(fn (string $file) => basename($file) === 'README.md');
+    protected static function getTypeClassname(string $name): string
+    {
+        return 'App\\DataCollections\\Types\\'.Str::studly($name);
     }
 }
