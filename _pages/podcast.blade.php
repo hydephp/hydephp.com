abdeaca2c78881e37e538736f9b8b28d940f1c47
Caen De Silva <caen@desilva.se>
2024-09-20 17:19:44 +0200

    Create podcast.blade.php

---
+++ _pages/podcast.blade.php
@@ -0,0 +1,9 @@
+@extends('hyde::layouts.app')
+@section('content')
+@php($title = "Podcast")
+
+<main class="mx-auto max-w-7xl py-16 px-8">
+    <h1 class="text-center text-3xl font-bold">Podcast</h1>
+</main>
+
+@endsection
