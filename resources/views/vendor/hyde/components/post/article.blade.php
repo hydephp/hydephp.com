587dbd589b8ede6350ab8f329c45c2662b2adca5
Caen De Silva <caen@desilva.se>
2024-12-27 21:24:03 +0100

    Add referrer and link

--- resources/views/vendor/hyde/components/post/article.blade.php
+++ resources/views/vendor/hyde/components/post/article.blade.php
@@ -14,7 +14,7 @@
         </div>
        @if($page->matter('guest_post') && $page->matter('canonical_guest_post_url'))
             <p class="text-sm text-gray-600 dark:text-gray-400 italic mb-0 mt-2">
-                Originally posted at <a href="{{ $page->matter('canonical_guest_post_url') }}" class="text-indigo-600 dark:text-indigo-400 hover:underline" rel="canonical nofollow external" target="_blank">
+                Originally posted at <a href="{{ $page->matter('canonical_guest_post_url') }}?ref=hydephp.com" class="text-indigo-600 dark:text-indigo-400 hover:underline" rel="canonical nofollow external" target="_blank">
                     {{ parse_url($page->matter('canonical_guest_post_url'), PHP_URL_HOST) }}
                 </a>
             </p>
