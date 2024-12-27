4ddce650d29d5bc0a4dab41c187568e0f518cc1e
Caen De Silva <caen@desilva.se>
2024-12-27 21:19:18 +0100

    Highlight guest posts

--- resources/views/vendor/hyde/components/article-excerpt.blade.php
+++ resources/views/vendor/hyde/components/article-excerpt.blade.php
@@ -5,12 +5,17 @@
         <meta itemprop="url" content="{{ Hyde::url('posts/' . $post->identifier) }}">
     @endif
 
-    <header>
+    <header class="flex items-center justify-between">
         <a href="{{ $post->getRoute() }}" class="block w-fit">
             <h2 class="text-2xl font-bold text-gray-700 hover:text-gray-900 dark:text-gray-200 dark:hover:text-white transition-colors duration-75">
                 {{ $post->data('title') ?? $post->title }}
             </h2>
         </a>
+        @if($post->data('guest_post'))
+            <span class="inline-block px-2 py-1 text-sm bg-indigo-100 text-indigo-800 dark:bg-indigo-900 dark:text-indigo-200 rounded-md mt-2">
+                Guest Post
+            </span>
+        @endif
     </header>
 
     <footer>
@@ -21,11 +26,20 @@
         @endisset
         @isset($post->author)
             <span itemprop="author" itemscope itemtype="https://schema.org/Person">
-            <span class="opacity-75">by</span>
-            <span itemprop="name">
-                {{ $post->author->name ?? $post->author->username }}
+                <span class="opacity-75">by</span>
+                @if($post->data('guest_post'))
+                    <span class="text-indigo-600 dark:text-indigo-400">guest author</span>
+                @endif
+                <span itemprop="name">
+                    @if($post->author->website)
+                        <a href="{{ $post->author->website }}" class="hover:underline" rel="{{ $post->data('guest_post') ? 'author nofollow external' : 'author' }}" target="{{ $post->data('guest_post') ? '_blank' : '_self' }}">
+                    @endif
+                    {{ $post->author->name ?? $post->author->username }}
+                    @if($post->author->website)
+                        </a>
+                    @endif
+                </span>
             </span>
-        </span>
         @endisset
     </footer>
 
