1a22cd7292257443766954329e4fb76489799469
Caen De Silva <caen@desilva.se>
2025-05-17 22:42:39 +0200

    Tweak spacing

--- resources/views/vendor/hyde/components/post/article.blade.php
+++ resources/views/vendor/hyde/components/post/article.blade.php
@@ -111,8 +111,8 @@
                 
                 <div class="mt-5 text-center">
                     <div class="text-sm text-gray-500 dark:text-gray-400">
-                        <p>Want to contribute to HydePHP? We'd love to hear from you!</p>
-                        <a href="https://hydephp.com/posts/guest-posts/" class="inline-flex items-center text-indigo-600 dark:text-indigo-400 hover:text-indigo-800 dark:hover:text-indigo-300 font-medium mt-1">
+                        <p>Want to contribute a post to HydePHP? We'd love to hear from you!</p>
+                        <a href="https://hydephp.com/posts/guest-posts/" class="inline-flex items-center text-indigo-600 dark:text-indigo-400 hover:text-indigo-800 dark:hover:text-indigo-300 font-medium mt-0">
                             <svg class="w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                             </svg>
