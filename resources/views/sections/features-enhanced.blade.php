216610a17c7f278dd781aa4f59d5648d784fa801
Emma De Silva <emma@desilva.se>
2025-09-28 15:35:21 +0200

    Fix border issue

--- resources/views/sections/features-enhanced.blade.php
+++ resources/views/sections/features-enhanced.blade.php
@@ -153,7 +153,7 @@
     </div>
     
     <!-- Wave transition -->
-    <svg class="absolute bottom-0 w-full text-white fill-current transform rotate-180" viewBox="0 0 1400 74" xmlns="http://www.w3.org/2000/svg">
+    <svg class="absolute bottom-0 w-full text-white fill-current transform rotate-180" viewBox="0 0 1400 74" xmlns="http://www.w3.org/2000/svg" style="margin-bottom: -1px;">
         <path d="M0 24C87.243 11.422 173.12 5.133 257.633 5.133 468.305 5.133 578.027 74 700 74c136.015 0 290.882-96.208 481.234-68.867C1268.807 17.71 1341.73 24 1400 24v50H0V24z" />
     </svg>
 </section>
