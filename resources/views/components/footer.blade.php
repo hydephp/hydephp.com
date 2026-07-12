710d8426be1d88e05afdcd0a516caaa65b2551c0
Emma De Silva <emma@desilva.se>
2026-07-12 20:01:04 +0200

    Implement the links in the footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,10 +2,10 @@
   <div class="mx-auto flex max-w-[1160px] flex-wrap items-center gap-6 px-7">
     <span>Site proudly built with HydePHP 🎩</span>
     <div class="ml-auto flex gap-5">
-      <a class="text-[#a49cba] no-underline hover:text-white" href="#">GitHub</a>
-      <a class="text-[#a49cba] no-underline hover:text-white" href="#">Discord</a>
-      <a class="text-[#a49cba] no-underline hover:text-white" href="#">RSS</a>
-      <a class="text-[#a49cba] no-underline hover:text-white" href="#">Legal</a>
+      <a class="text-[#a49cba] no-underline hover:text-white" href="https://github.com/hydephp/hyde">GitHub</a>
+      <a class="text-[#a49cba] no-underline hover:text-white" href="https://discord.hydephp.com">Discord</a>
+      <a class="text-[#a49cba] no-underline hover:text-white" href="{{ Hyde::url('feed.xml') }}">RSS</a>
+      <a class="text-[#a49cba] no-underline hover:text-white" href="{{ \Hyde\Foundation\Facades\Routes::get('license') ?? '/license' }}">Legal</a>
     </div>
   </div>
 </footer>
