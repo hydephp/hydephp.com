3081d3263480e6f49a045086ca01d17887651ed6
Emma De Silva <emma@desilva.se>
2026-08-20 07:27:48 +0200

    Update index.blade.php

--- _pages/index.blade.php
+++ _pages/index.blade.php
@@ -9,7 +9,7 @@ $statistics = \App\Support\HomepageStatistics::cards();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>The static site generator for PHP.</title>
+  <title>HydePHP · The static site generator for PHP.</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,opsz,wght@0,5..1200,400..900;1,5..1200,400..900&family=Instrument+Sans:ital,wght@0,400..700;1,400..700&family=JetBrains+Mono:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
